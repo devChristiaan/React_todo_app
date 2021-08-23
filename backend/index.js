8985ec41ad9ede487c47a7e660ea444d24a2b790
Christiaan Hougaard <cwhougaard@gmail.com>
2021-08-23 14:40:28 -0400

    basic server ready

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
-app = express();
+//Instantiate the app
+const app = express();
+dotenv.config();
 
+//Global Middleware Setup
 app.use(cors());
-app.use()
+app.use(express.json())
+
+const port = process.env.PORT || 9001;
+
+app.listen((req, res) => {
+  console.log(`Server listening on port ${port}`);
+})
