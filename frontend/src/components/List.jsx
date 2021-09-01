adbdbb21db5957a5e28fd0317d21d25a880c9736
Christiaan Hougaard <cwhougaard@gmail.com>
2021-08-31 22:51:35 -0400

    added edit field to list item

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom'
-import { TableCell, TableRow } from "@material-ui/core"
+import { TableCell, TableRow, TextField } from "@material-ui/core"
 import CreateIcon from '@material-ui/icons/Create';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { deleteList } from '../functions/helpers.js'
@@ -7,21 +8,45 @@ import { deleteList } from '../functions/helpers.js'
 
 const List = (props) => {
 
+  const [edit, setEdit] = useState(false)
+
   const history = useHistory()
 
   const deleteCurrentList = (id, url) => {
     deleteList(id, url)
     history.go(0)
   }
+
+  const editListItem = () =>{
+    if(edit){
+      setEdit(false)
+    } else {
+      setEdit(true)
+    }
+    
+  }
   
+  function renderElement() {
+    if(!edit){
+      return(
+        <TableCell component="th" scope="row" onClick={props.openList}>
+        {props.title}
+      </TableCell>
+      )
+    } else {
+      return(
+        <TableCell>
+          <TextField id="standard-basic" label={props.title}/>
+        </TableCell>
+      )
+    }
+  } 
 
   return (
     <TableRow key={props.key}>
-      <TableCell component="th" scope="row" onClick={props.openList}>
-        {props.title}
-      </TableCell>
+      { renderElement() }
       <TableCell align="right">
-        <CreateIcon/>
+        <CreateIcon onClick={editListItem}/>
         <DeleteIcon onClick={() => deleteCurrentList(props.listId, props.url, history)}/>
       </TableCell>
     </TableRow>
