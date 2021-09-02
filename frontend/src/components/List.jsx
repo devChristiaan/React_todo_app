ed7542f8f363c6175570f6c72c4545646bdcf383
Christiaan Hougaard <cwhougaard@gmail.com>
2021-09-01 20:01:22 -0400

    added edit func to list comp and added axios functions to the helpers js

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -3,18 +3,29 @@ import { useHistory } from 'react-router-dom'
 import { TableCell, TableRow, TextField } from "@material-ui/core"
 import CreateIcon from '@material-ui/icons/Create';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { deleteList } from '../functions/helpers.js'
+import SaveIcon from '@material-ui/icons/Save';
+import { deleteList, renameList } from '../functions/helpers.js'
 
 
 const List = (props) => {
 
   const [edit, setEdit] = useState(false)
+  const [title, setTitle] = useState(props.title)
 
   const history = useHistory()
 
   const deleteCurrentList = (id, url) => {
     deleteList(id, url)
     history.go(0)
+  }
+
+  const handleListRename = (title, listId, url) => {
+    renameList(title, listId, url)
+    history.go(0)
+  }
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value)
   }
 
   const editListItem = () =>{
@@ -29,15 +40,27 @@ const List = (props) => {
   function renderElement() {
     if(!edit){
       return(
+        <>
         <TableCell component="th" scope="row" onClick={props.openList}>
         {props.title}
-      </TableCell>
+        </TableCell>
+          <TableCell align="right">
+          <CreateIcon onClick={editListItem}/>
+          <DeleteIcon onClick={() => deleteCurrentList(props.listId, props.url, history)}/>
+        </TableCell>
+      </>
       )
     } else {
       return(
+      <>
         <TableCell component="th" scope="row">
-          <TextField id="standard-basic" label={props.title}/>
+          <TextField onChange={(e) => handleTitleChange(e)}id="standard-basic" label={props.title}/>
         </TableCell>
+        <TableCell align="right">
+          <SaveIcon onClick={(e) => handleListRename(title, props.listId, props.url)}/>
+          <DeleteIcon onClick={() => deleteCurrentList(props.listId, props.url, history)}/>
+        </TableCell>
+      </>
       )
     }
   } 
@@ -45,10 +68,6 @@ const List = (props) => {
   return (
     <TableRow key={props.key}>
       { renderElement() }
-      <TableCell align="right">
-        <CreateIcon onClick={editListItem}/>
-        <DeleteIcon onClick={() => deleteCurrentList(props.listId, props.url, history)}/>
-      </TableCell>
     </TableRow>
   );
 }
