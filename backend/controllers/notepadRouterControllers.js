cc4cd625c7a5aba3db445139c3776532864b1804
Christiaan Hougaard <cwhougaard@gmail.com>
2021-09-09 20:07:39 -0400

    create the get all notes route and route controller

--- backend/controllers/notepadRouterControllers.js
+++ backend/controllers/notepadRouterControllers.js
@@ -1,5 +1,7 @@
 import { db } from './dbconnection.js'
 
+// @desc    Create a new Note
+// @route   POST /api/v1/notepad
 const createNote = (req, res) => {
   
   const query = `INSERT INTO notes (Title, Content) VALUES ('${req.body.Title}', '${req.body.Content}')`
@@ -14,4 +16,37 @@ const createNote = (req, res) => {
   })
 }
 
-export { createNote }
+// @desc    Get all notes
+// @route   GET /api/v1/notepad
+const getNote = (req, res) => {
+  // Need to update the function to take in the id of the note to be updated
+
+  const query = `SELECT * FROM notes`
+  
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send({error: 'Error creating note'})
+    } else {
+      res.status(200).send(JSON.stringify(result))
+    }
+  })
+}
+
+// @desc    Update a Note
+// @route   PATCH /api/v1/notepad
+const updateNote = (req, res) => {
+  
+  const query = `INSERT INTO notes (Title, Content) VALUES ('${req.body.Title}', '${req.body.Content}')`
+  
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send({error: 'Error creating note'})
+    } else {
+      res.status(200).send({success: 'Notepad created successfully'})
+    }
+  })
+}
+
+export { createNote, getNote, updateNote }
--- backend/routes/notepadRouter.js
+++ backend/routes/notepadRouter.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { createNote } from '../controllers/notepadRouterControllers'
+import { createNote, getNote } from '../controllers/notepadRouterControllers'
 
 const notepadRouter = express.Router()
 
 router.route('/')
-  .get()
+  .get(getNote)
   .patch()
   .post(createNote)
 
