be87143bd3b0e5a7bf1e70af6e73c3ddd01b4c0a
Christiaan Hougaard <cwhougaard@gmail.com>
2021-09-09 20:13:42 -0400

    fixed import statement and router decleration

--- backend/routes/notepadRouter.js
+++ backend/routes/notepadRouter.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { createNote, getNote } from '../controllers/notepadRouterControllers'
+import { createNote, getNote, updateNote } from '../controllers/notepadRouterControllers.js'
 
 const notepadRouter = express.Router()
 
-router.route('/')
+notepadRouter.route('/')
   .get(getNote)
-  .patch()
+  .patch(updateNote)
   .post(createNote)
 
 
