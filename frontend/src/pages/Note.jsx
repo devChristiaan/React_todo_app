51a3ba748f21ab6c292d24a633e625543d742c2b
Christiaan Hougaard <cwhougaard@gmail.com>
2021-09-14 19:19:38 -0400

    fix fetch error and added the form to display and update note. Also added save button and update fetch func

--- frontend/src/pages/Note.jsx
+++ frontend/src/pages/Note.jsx
@@ -1,30 +1,67 @@
 import React, { useState, useEffect } from "react";
-import { fetchNotes } from "../functions/notepadHelpers";
-import { Container } from '@material-ui/core';
+// import { fetchNotes } from "../functions/notepadHelpers";
+// import axios from "axios";
+import { Container, Paper } from '@material-ui/core';
 
 const Note = () => {
 
-  const url = process.env.REACT_APP_NOTEPADAPI;
+  const url = process.env.REACT_APP_NOTEPAD_API;
 
-  const [note, setNote] = useState()
-  const [title, setTitle] = useState()
+  const [note, setNote] = useState("Untitled")
+  const [title, setTitle] = useState("Message")
+  const [noteID, setNoteID] = useState("1")
 
+  
   useEffect(() => {
-    const getData = async () => {
+    const getData = async (url) => {
+      const response = await fetch(url, {
+          method: 'GET',
+      })
+      const data = await response.json()
 
-      const response = await fetchNotes(url);
-      const data = await response
+      setNote(data[0].Content)
+      setTitle(data[0].Title)
+      setNoteID(data[0].NoteID)
+  }
 
-      setNote(data.Content)
-      setTitle(data.Title)
-    }
-    getData();
+    getData(url);
   }, [url])
 
+  const handleTitlChange = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const handleNoteChange = (e) => {
+    setNote(e.target.value)
+  }
+
+  const saveNote = () => {
+    const data = {
+      Title: title,
+      Content: note,
+      NoteID: noteID
+    }
+
+    const postData = async (url, data) => {
+      const response = await fetch(url, {
+          method: 'PATCH',
+          body: JSON.stringify(data),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      })
+      const result = await response.json()
+      console.log(result)
+  }
+
+    postData(url, data)
+  }
+
   return(
-    <Container>
-      <h1>{title}</h1>
-      <textarea>{note}</textarea>
+    <Container component={Paper}>
+      <input value={title} onChange={(e) => handleTitlChange(e)}></input>
+      <textarea value={note} onChange={(e) => handleNoteChange(e)}></textarea>
+      <button type="submit" onClick={saveNote}>Save</button>
     </Container>
   )
 }
