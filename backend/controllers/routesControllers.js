a090406677e62cef3c5a377449f1360f8861cda8
Christiaan Hougaard <cwhougaard@gmail.com>
2021-09-26 15:57:58 -0400

    refactored getlists, getlistItems and add list controller funcs to use pooling.

--- backend/controllers/routesControllers.js
+++ backend/controllers/routesControllers.js
@@ -3,53 +3,57 @@ import { dbPool } from './dbconnection.js'
 // @desc    Get all lists
 // @route   GET /api/v1/lists
 const getLists = (req, res, next) => {
-  try {
-    const query = `SELECT * FROM lists`
 
-    db.query(query, (err, result) => {
+  dbPool.getConnection((err, connection) => {
+    const query = 'SELECT * FROM lists'
+
+    connection.query(query, (err, result) => {
       if (err) {
         res.status(404).send({notFound:"No Lists Found. Please Create a List"})
+        connection.release()
       } else {
         res.status(200).send(JSON.stringify(result))
+        connection.release()
       }
     })
-  } catch (err) {
-    res.status(500).send({error: err.message})
-  }
-
+  })
 }
 
 // @desc    Get all list items
 // @route   GET /api/v1/listitems/:id
 const getListItems = (req, res, next) => {
   
-  try {
-    const query = `select * from item INNER JOIN list_items ON list_items.ListID = ? && item.ItemID = list_items.ItemID`
+  dbPool.getConnection((err, connection) => {
+    const query = 'select * from item INNER JOIN list_items ON list_items.ListID = ? && item.ItemID = list_items.ItemID'
 
-    db.query(query, [req.params.id], (err, result) => {
+    connection.query(query, [req.params.id], (err, result) => {
       if (err) {
         res.status(404).send({notFound:"No Lists Items found. Please Create an Item"})
+        connection.release()
       } else {
         res.status(200).send(JSON.stringify(result))
+        connection.release()
       }
     })
-  } catch (err) {
-    res.status(500).send({error: err.message})
-  }
+  })
 }
     
 // @desc    Add a new List
 // @route   POST /api/v1/lists
 const addList = (req, res, next) => {
 
-  const query = `INSERT INTO lists (Title, Location) VALUES ("${req.body.content}", 100);`
-    db.query(query, (err, result) => {
+  dbPool.getConnection((err, connection) => {
+    const query = 'INSERT INTO lists (Title, Location) VALUES (?, 100);'
+    connection.query(query, [req.body.content], (err, result) => {
       if (err) {
         res.status(404).send({notFound:"No Lists found. Please Create a List"})
+        connection.release()
       } else {
         res.status(200).send({success:"List added successfully"})
+        connection.release()
       }
     })
+  })
 }
 
 // @desc    Add item and assign item to list
